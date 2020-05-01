566ff39be0be5eb9b1f9597de29f2b543d0f2208
ZiimaBlue <63270023+ZiimaBlue@users.noreply.github.com>
2020-05-01 19:32:21 -0300

    Ventana Contacto / Responsive Tabletas
    Hice ventana de contacto
    Hice responsive para Tabletas y pantallas menores a 1024px

--- js/index.js
+++ js/index.js
@@ -1,4 +1,7 @@
 var nombre = prompt('Ingrese su nombre:');
+if (nombre == null || nombre == ""){
+	nombre = 'tu';
+}
 
 var elemento1 = document.getElementById('bienvenida');
 elemento1.innerHTML = '<h2 id=nombre> Bienvenid@ '+nombre+'</h2>'
--- js/menu.js
+++ js/menu.js
@@ -1,11 +1,18 @@
-// var edad = prompt('Ingrese su edad:');
 var saldo = prompt('Ingrese su saldo disponible:');
+var edad = prompt('Ingrese su edad:');
 
-var elemento2 = document.getElementById('saldo');
-elemento2.innerHTML = '<h2 id=saldoDis> $ '+saldo+'</h2>'
+if (saldo == null || saldo == ""){
+	saldo = '0';
+}
 
 
-let preciosP = new Array();
+var elemento1 = document.getElementById('saldo');
+elemento1.innerHTML = '<h2 id=saldoDis> $ '+saldo+'</h2>'
+
+var edades = new Array();
+edades = document.getElementsByClassName('edad');
+
+var preciosP = new Array();
 preciosP = document.getElementsByClassName ('precio');
 
 
@@ -14,22 +21,21 @@ console.log('El saldo actual es de: '+saldo);
 
 for (var i = 0; i < preciosP.length; i++) {
 
-	console.log(preciosP[i].textContent);
-
 	if(parseInt(saldo) < parseInt(preciosP[i].textContent)){
 
-		preciosP[i].style.color='#ad230e';//
+		preciosP[i].style.color='#ad230e';
+
 	}
 }
-	
 
-// for (i = 0; i < document.getElementsByClassName('precio').length; i++){
-        
+for (var i = 0; i < edades.length; i++) {
+	console.log(edades[i].textContent);
+	if(parseInt(edad) < 18 & parseInt(edades[i].textContent) == 18){
 
-//     var valor = document.getElementsByClassName('precio')[i];
-    
-//     var valor2 = valor.textContent;
-    
-//     alert(valor2);
-        
-// }      
+		preciosP[i].style.textDecoration="line-through";
+		preciosP[i].style.color='#ad230e';
+	}
+}
+
+
+	    
